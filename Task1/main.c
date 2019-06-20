ce02690654fe450adf1797490e512c762b5be903
oleggtitoff <46402619+oleggtitoff@users.noreply.github.com>
2019-06-20 12:37:59 +0300

    Add, Sub
    math.h removed.
    Add and Sub functions added.

--- Task1/main.c
+++ Task1/main.c
@@ -1,28 +1,43 @@
 #include <stdint.h>
-#include <math.h>
+#include <stdio.h>
 
-#define FRACTIONAL_BITS 16
+#define FRACTIONAL_BITS 31
 
-uint32_t floatToFixed32(float x)
+int32_t floatToFixed32(float x)
 {
-	return (uint32_t)(round(x * (1 << FRACTIONAL_BITS)));
+	return (int32_t)(x * ((1 << FRACTIONAL_BITS) - 1));
 }
 
-float fixed32ToFloat(uint32_t x)
+float fixed32ToFloat(int32_t x)
 {
-	return (float)x / (float)(1 << FRACTIONAL_BITS);
+	return (float)x / (float)((1 << FRACTIONAL_BITS) - 1);
+}
+
+int32_t Add(int32_t x, int32_t y)
+{
+	if ((y > 0 && x > INT32_MAX - y) || (y < 0 && x < INT32_MIN - y))
+
+	{
+		printf("Add error: overfloat\n");
+		system("pause");
+		exit(0);
+	}
+
+	return x + y;
+}
+
+int32_t Sub(int32_t x, int32_t y)
+{
+	if ((x > 0 && y < INT32_MAX - x) || (x < 0 && y > INT32_MIN - x))
+	{
+		printf("Sub error: overfloat\n");
+		system("pause");
+		exit(0);
+	}
+
+	return x - y;
 }
 /*
-uint32_t Add(uint32_t x, uint32_t y)
-{
-
-}
-
-uint32_t Sub(uint32_t x, uint32_t y)
-{
-
-}
-
 uint32_t Mul(uint32_t x, uint32_t y)
 {
 
