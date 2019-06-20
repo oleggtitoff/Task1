987b724ca1f61d632906a25b15b04f4da95aea17
oleggtitoff <46402619+oleggtitoff@users.noreply.github.com>
2019-06-20 15:43:54 +0300

    Update main.c
    Updated floatToFixed32, Sub.
    Added Mul, Mac, MSub.

--- Task1/main.c
+++ Task1/main.c
@@ -5,6 +5,13 @@
 
 int32_t floatToFixed32(float x)
 {
+	if (x >= 1 || x < -1)
+	{
+		printf("Converting error: Float must be < 1 and >= -1\n");
+		system("pause");
+		exit(0);
+	}
+
 	return (int32_t)(x * ((1 << FRACTIONAL_BITS) - 1));
 }
 
@@ -15,7 +22,7 @@ float fixed32ToFloat(int32_t x)
 
 int32_t Add(int32_t x, int32_t y)
 {
-	if ((y > 0 && x > INT32_MAX - y) || (y < 0 && x < INT32_MIN - y))
+	if ((x > 0 && y > INT32_MAX - x) || (x < 0 && y < INT32_MIN - x))
 
 	{
 		printf("Add error: overfloat\n");
@@ -28,7 +35,7 @@ int32_t Add(int32_t x, int32_t y)
 
 int32_t Sub(int32_t x, int32_t y)
 {
-	if ((x > 0 && y < INT32_MAX - x) || (x < 0 && y > INT32_MIN - x))
+	if ((x > 0 && -y > INT32_MAX - x) || (x < 0 && -y < INT32_MIN - x))
 	{
 		printf("Sub error: overfloat\n");
 		system("pause");
@@ -37,32 +44,28 @@ int32_t Sub(int32_t x, int32_t y)
 
 	return x - y;
 }
-/*
-uint32_t Mul(uint32_t x, uint32_t y)
-{
 
+int32_t Mul(int32_t x, int32_t y)
+{
+	if (x == 0x80000000 && y == 0x80000000)
+	{
+		printf("Mul error: overfloat\n");
+		system("pause");
+		exit(0);
+	}
+
+	return (int32_t)(((int64_t)x * y) >> 31);
 }
 
-uint32_t Mac(uint32_t x, uint32_t y)
+int32_t Mac(int32_t x, int32_t y, int32_t acc)
 {
-
+	return Add(Mul(x, y), acc);
 }
 
-uint32_t MSub(uint32_t x, uint32_t y)
+int32_t MSub(int32_t x, int32_t y, int32_t sub)
 {
-
+	return Sub(Mul(x, y), sub);
 }
-
-uint32_t LeftShift(uint32_t x)
-{
-
-}
-
-uint32_t RightShift(uint32_t x)
-{
-
-}
-*/
 
 int main()
 {
